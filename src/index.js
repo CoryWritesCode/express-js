81f858132f189dd080b5997e2e61e6a0020b1d43
CoryWritesCode <corycunningham2010@gmail.com>
2018-11-28 07:09:28 -0600

    in again

--- src/index.js
+++ src/index.js
@@ -4,12 +4,36 @@ const fs = require('fs');
 const bodyParser = require('body-parser');
 
 let app = express();
+let i = 0
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.post('/form', (req, res) => {
-  fs.appendFileSync(path.join(__dirname, '../public/formsubmissions'), `Name: ${req.body.name}\n`);
-  fs.appendFileSync(path.join(__dirname, '../public/formsubmissions'), `Email: ${req.body.email}\n`);
+
+  let formPath = path.join(__dirname, '../server/formsubmissions/forms.json');
+  let name = req.body.name;
+  let email = req.body.email;
+
+  fs.readFile(formPath, (err, data) => {
+    if (err) throw err;
+
+    var json = JSON.parse(data);
+
+    console.log(json);
+
+    json[`name${i}`] = `${name}`;
+    json[`email${i}`] = `${email}`;
+
+    console.log(json);
+
+    fs.writeFile(formPath, JSON.stringify(json), (err) => {
+      if (err) throw err;
+    });
+    
+    i++
+
+  })
+
   res.send('Thank you for your trust');
 })
 
